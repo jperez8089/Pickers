7dacccc5eaadd7079ca76f9c11995c8d39cf01d2
Javier Perez <jperez8089@e104-m-21.mchenry.edu>
2018-02-19 15:38:17 -0600

    initial commit

---
+++ PickersJavierP/SingleComponentViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SingleComponentViewController.swift
+//  PickersJavierP
+//
+//  Created by Javier Perez on 2/19/18.
+//  Copyright © 2018 Javier Perez. All rights reserved.
+//
+
+import UIKit
+
+class SingleComponentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
